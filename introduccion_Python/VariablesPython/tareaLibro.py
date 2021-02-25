7b68a615ae9ec6a81428d091b17d0787f4fac4ec
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-02-24 21:58:45 -0600

    séptimo ejercicio

---
+++ introduccion_Python/VariablesPython/tareaLibro.py
@@ -0,0 +1,8 @@
+titulo = input("Proporcioa el titulo: ")
+autor = input("Proporciona el autor: ")
+
+# Opcion 1
+# print("{} fue escrito por {}".format(titulo, autor))
+
+# Opcion 2
+print(titulo+" fue escrito por "+autor)
