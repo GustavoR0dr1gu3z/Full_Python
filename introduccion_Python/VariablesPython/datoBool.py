6c425d26780408579ef0b2801573868b79cf4292
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-02-24 21:40:54 -0600

    quinto ejercicio

---
+++ introduccion_Python/VariablesPython/datoBool.py
@@ -0,0 +1,19 @@
+x = True
+print(x)
+
+y = False
+print(y)
+
+num1 = 1
+num2 = 2
+
+# res = num1>num2
+res = num1<num2
+
+print(res)
+
+
+if(num1<num2):
+    print("Num1 es menor que Num2")
+else:
+    print("Num2 es menor que Num1")
