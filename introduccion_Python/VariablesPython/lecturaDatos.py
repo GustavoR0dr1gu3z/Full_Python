f2ac5213815c7910e089cb8f9edd86e6dd8844f2
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-02-24 21:46:59 -0600

    sexto ejercicio

---
+++ introduccion_Python/VariablesPython/lecturaDatos.py
@@ -0,0 +1,8 @@
+resultado = input("Haber: ")
+print("Resultado: ",resultado)
+
+num1 = int(input("Digite un numero: "))
+num2 = int(input("Digite un numero: "))
+
+res = num1+num2
+print("La suma es: ",res)
