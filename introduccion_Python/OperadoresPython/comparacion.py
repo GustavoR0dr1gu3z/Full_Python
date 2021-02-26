9751e0449aef4a9efcbf8528c74bb1587759be6b
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-02-25 22:08:22 -0600

    decimo ejercicio

---
+++ introduccion_Python/OperadoresPython/comparacion.py
@@ -0,0 +1,37 @@
+a = 4
+# b = 2
+
+# resultado = (a==b)
+# print(resultado)
+
+# resultado = (a!=b)
+# print(resultado)
+
+# resultado = (a>b)
+# print(resultado)
+
+# resultado = (a>=b)
+# print(resultado)
+
+# resultado = (a<b)
+# print(resultado)
+
+# resultado = (a<=b)
+# print(resultado)
+
+
+if(a%2 == 0):
+    print("Es par")
+else:
+    print("Es impar")
+    
+    
+    
+edadLi = 18
+edadPer = 5
+
+if(edadPer>=edadLi):
+    print("Es un adulto")
+else:
+    print("Es menor de edad")
+    
