f64cb4fe473fa9d31b6bb0217304379caa02b6d9
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-02-25 22:22:07 -0600

    onceavo ejercicio

---
+++ introduccion_Python/OperadoresPython/opLogicos.py
@@ -0,0 +1,24 @@
+a = int(input("Digite un valor: "))
+min = 0
+max = 5
+
+if(a>=min and a<=max):
+    print("En el rango")
+else:
+    print("Fuera de rango")
+    
+    
+vacaciones = False
+descanso = False
+    
+if(vacaciones or descanso):
+    print("Ir al parque")
+else:
+    print("Nel")
+    
+    
+if(not(vacaciones or descanso)):
+    print("ir al parque")
+else:
+    print("NEL")
+    
