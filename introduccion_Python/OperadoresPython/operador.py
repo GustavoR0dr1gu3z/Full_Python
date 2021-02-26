97c0de79ae0520a560abd40e6b65e8c42e010d8c
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-02-25 21:47:48 -0600

    octavo ejercicio

---
+++ introduccion_Python/OperadoresPython/operador.py
@@ -0,0 +1,32 @@
+a = 3
+b = 2 
+# Operador suma
+suma = a+b
+print("Suma: ",suma)
+
+# Operador resta
+resta = a-b
+print("Resta: ",resta)
+
+
+# Operador multiplicacion
+mul = a*b
+print("Multiplicacion: ",mul)
+
+
+# Operador division
+div = a/b
+print("Division: ",div)
+
+# Operador modulo
+mod = a%b
+print("Modulo: ",mod)
+
+# Operador exponente
+exp = a**b
+print("Exponente: ",exp)
+
+
+
+
+
