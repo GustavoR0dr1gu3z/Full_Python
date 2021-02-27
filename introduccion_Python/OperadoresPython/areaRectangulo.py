20079e018c18bb351ab99f3ab5793d8dc11e766c
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-02-26 19:33:42 -0600

    ejercicio 12

---
+++ introduccion_Python/OperadoresPython/areaRectangulo.py
@@ -0,0 +1,8 @@
+alto = int(input("Digite el alto: "))
+ancho = int(input("Digite el ancho: "))
+
+Area = (alto*ancho)
+Perimetro = ((alto+ancho)*2)
+
+print("Area: ",Area)
+print("Perimetro: ",Perimetro)
