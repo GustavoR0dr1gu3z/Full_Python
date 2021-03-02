bec54d82e2991439359a2603d00d8cb1fcd1f4f0
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-03-01 21:51:52 -0600

    Ejercicio 15

---
+++ introduccion_Python/SentenciaIF_Python/sentencia_if_else.py
@@ -0,0 +1,23 @@
+condicion = 10
+
+if condicion == True:
+    print("La condicion es verdadera")
+elif condicion == False:
+    print("La condicion es falsa")    
+else:
+    print("xd")
+    
+    
+numero = int(input("Digite un numero entre 1 y 3: "))
+if numero == 1:
+    numeroTexto = "Numero Uno"
+elif numero == 2:
+    numeroTexto = "Numero Dos"
+elif numero == 3:
+    numeroTexto = "Numero Tres"
+else:
+    numeroTexto = "Numero Fuera de Rango"
+    
+print("Numero proporcionado: {}".format(numeroTexto))
+
+    
