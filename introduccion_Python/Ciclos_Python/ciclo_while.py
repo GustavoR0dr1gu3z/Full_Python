4946f10307fb0407177c94a7f906ba66c8fdf4de
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-03-02 21:21:18 -0600

    Ejercicio 19

---
+++ introduccion_Python/Ciclos_Python/ciclo_while.py
@@ -0,0 +1,14 @@
+"""
+condicion = False
+
+while condicion:
+    print("Ejecutando ciclo while")
+else:
+    print("No ejecutando nada")
+"""
+i = 0
+while i < 3:
+    print("Ejecutando while {}".format(i))
+    i+=1
+else:
+    print("Fin Ciclo While")    
