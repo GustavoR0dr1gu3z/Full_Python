118073f739d3ea26f229698f246efdc0ebcc7bee
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-03-02 21:54:17 -0600

    Ejercicio 22

---
+++ introduccion_Python/Ciclos_Python/break.py
@@ -0,0 +1,17 @@
+for letra in "Holanda":
+    if letra =='a':
+        print(letra)        
+else:
+    print("Fin ciclo for")
+
+
+
+
+
+
+
+
+for i in range(6):
+    if i%2 == 0:
+        continue
+    print("Valores Pares: {}".format(i))
