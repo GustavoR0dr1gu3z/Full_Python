e9afc664fed2aa427ab2592c9de711c0b70030a8
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-03-03 22:27:44 -0600

    Ejercicio 23

---
+++ introduccion_Python/Colecciones_Python/lista.py
@@ -0,0 +1,10 @@
+#Creando la lista
+nombreLista = ["Gus","Mich","Mari","Renata"]
+# Imprimir la lista
+print(nombreLista)
+# Para saber el largo de la lista
+print(len(nombreLista))
+# Acceder a un elemento
+print(nombreLista[0])
+# Navegacion inversa
+print(nombreLista[-1])
