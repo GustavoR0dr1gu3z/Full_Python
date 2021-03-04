693c1f57bc8b1b86bf55e1a6c93c394384988220
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-03-03 22:35:04 -0600

    Ejercicio 23 parte2

--- introduccion_Python/Colecciones_Python/lista.py
+++ introduccion_Python/Colecciones_Python/lista.py
@@ -8,3 +8,22 @@ print(len(nombreLista))
 print(nombreLista[0])
 # Navegacion inversa
 print(nombreLista[-1])
+
+# Imprimir rango
+print(nombreLista[0:2]) # Sin incluir el 2
+# Imprimir los elementos de inicio hasta i
+print(nombreLista[:3]) #Sin incluir el 3
+# Imprimir los elementos hasta el final
+print(nombreLista[1:])
+# Cambiar los elementos de una lista
+nombreLista[3]="Reni"
+print(nombreLista)
+# Iterar la lista
+for i in nombreLista:
+    print(i)
+# Revisar si existe un elemento dentro de la lista
+if "Renid" in nombreLista:
+    print("Reni si existe en la lista")
+else:
+    print("El elemento buscado no existe en la lista"        )
+
