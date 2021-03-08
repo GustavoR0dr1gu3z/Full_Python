7a0d9e35a05ea536a85d36b82f4ca3d6e8a16879
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-03-07 19:44:29 -0600

    Ejercicio 30

---
+++ introduccion_Python/Funciones_Python/funciones_argumentos.py
@@ -0,0 +1,8 @@
+# Parametro es una variable definida entre parentesis de la funcion
+# Argumento (arg) valor enviado a la funcion
+
+def funcion_arg(nombre, apellido):
+    print("El nombre recibido es: ",nombre)
+    print("El apellido recibido es: ",apellido)
+
+funcion_arg("Gustavo", "Calzada")
