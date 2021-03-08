5a68493e0befe77e5465d9e3c04b6200cb48e9c3
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-03-07 19:52:02 -0600

    Ejercicio 31

---
+++ introduccion_Python/Funciones_Python/funciones_return.py
@@ -0,0 +1,16 @@
+def suma(a, b):
+    suma = a+b
+    return suma  
+    
+print("El resultado es: ",suma(5, 3))
+
+
+def suma2(a, b):
+    return a+b
+    
+print("El resultado es; ",suma2(5, 2))
+
+def suma3(a=1, b=0):
+    return a+b
+    
+print("El resultado es; ",suma3(1,89))
