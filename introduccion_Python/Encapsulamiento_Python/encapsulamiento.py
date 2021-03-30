fffac250edb8cdb2c09f23bfa9c63627ca3a3bf1
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-03-29 21:28:32 -0600

    Ejercicio 33

--- introduccion_Python/Encapsulamiento_Python/encapsulamiento.py
+++ introduccion_Python/Encapsulamiento_Python/encapsulamiento.py
@@ -1,17 +1,22 @@
 class Persona:
-    def __init__(self, nombre):
+    def __init__(self, nombre, edad):
         self.__nombre = nombre  # __ es para variable privada
+        self.__edad = edad
 
-# METODOS GET Y SET
+# METODOS GET Y SET -------NOMBRE--------
     def get_nombre(self):
         return self.__nombre
         
     def set_nombre(self, nombre):
         self.__nombre = nombre
+# METODOS GET Y SET ---------EDAD----------
+    def get_edad(self):
+        return self.__edad
 
-p1 = Persona("Gus")
+    def set_edad(self,edad):
+        self.__edad = edad 
+
+p1 = Persona("Gus", 21)
 #print(p1.__nombre)  Marca error
 print(p1.get_nombre())
-
-p1.set_nombre("Gustavo")
-print(p1.get_nombre())
+print(p1.get_edad())
