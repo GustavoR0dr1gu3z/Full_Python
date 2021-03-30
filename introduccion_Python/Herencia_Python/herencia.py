9fd252477786bc58b94fd64c24f4a7e0dcd39d04
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-03-29 21:58:37 -0600

    Ejercicio 35

---
+++ introduccion_Python/Herencia_Python/herencia.py
@@ -0,0 +1,16 @@
+class Persona:
+    def __init__(self, nombre, edad): # Nombre, Edad
+        self.nombre = nombre
+        self.edad = edad
+
+    def __str__(self): # Sobtr Escribir
+        return "Nombre:" + self.nombre + " , edad: "+ str(self.edad)
+
+class Empleado(Persona): # Empleado HEREDA de Persona
+    def __init__(self, nombre, edad, sueldo): # Nombre, Edad, Sueldo
+        super().__init__(nombre, edad)
+        self.sueldo = sueldo
+
+
+persona = Persona("Gustavo", 21)
+print(persona)
