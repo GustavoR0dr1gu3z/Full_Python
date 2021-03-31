c82a9d376d06e99c85d56b8e7f4560d7532ffe6d
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-03-30 20:30:44 -0600

    Ejercicio 36

---
+++ introduccion_Python/Modulos_Python/Funciones_Python/test_aritmetica.py
@@ -0,0 +1,13 @@
+# from modulos import modulo_aritmetica as ma
+# from modulos.modulo_aritmetica as ma
+import modulo_aritmetica as ma
+
+resultado = ma.sumar(1,2)
+print(resultado)
+resultado = ma.restar(1,2)
+print(resultado)
+resultado = ma.multiplicar(1,2)
+print(resultado)
+resultado = ma.dividir(1,2)
+print(resultado)
+
