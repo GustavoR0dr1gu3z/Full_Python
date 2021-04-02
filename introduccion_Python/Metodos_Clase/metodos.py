17837c254cfe66d66f6c483a4b4f925a9b72878a
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-01 21:57:45 -0600

    Ejercicio 42

---
+++ introduccion_Python/Metodos_Clase/metodos.py
@@ -0,0 +1,30 @@
+class MiClase:
+    
+
+    variable_clase = "Variable de Clase"
+
+    def __init__(self):
+        self.variable_instancia = "Variable de Instancia"
+
+    @staticmethod 
+    def metodo_estatico():
+        print("Método Estático")
+        print(MiClase.variable_clase)
+        # Desde un método estatico no podemos acceder a una variable de instancia
+        # print(MiClase.variable_instancia)
+
+    @classmethod
+    def metodo_clase(cls):    
+        print("Metodo de Clase"+str(cls))
+        print(cls.variable_clase)
+        # No podemos acceder a la variable de instancia desde un contexto estático
+        # print(cls.variable_instancia)
+
+    def metodo_instancia(self):
+        self.metodo_estatico()
+        self.metodo_clase()
+
+MiClase.metodo_estatico()
+MiClase.metodo_clase()
+objeto1 = MiClase()
+objeto1.metodo_instancia()
