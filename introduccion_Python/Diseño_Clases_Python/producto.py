dc328e5ce7024b5b2d39d394674891145c54a5f5
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-04-12 20:58:01 -0500

    Ejercicio 44

--- introduccion_Python/Diseño_Clases_Python/producto.py
+++ introduccion_Python/Diseño_Clases_Python/producto.py
@@ -1,6 +1,11 @@
 class Producto:
-    contador_producto = 0
+    contador_productos = 0
 
-    def __init__(self):
-        pass
-    
+    def __init__(self, nombre, precio):
+        Producto.contador_productos += 1 # Contador para cada vez que agregamos un objeto
+        self.__id_producto = Producto.contador_productos # El ID, será el contador
+        self.__nombre = nombre
+        self.__precio = precio
+
+    def __str__(self):
+        return  "ID Producto: "+self.__id_producto+", Nombre: "+self.__nombre+", Precio: "+str(self.__precio)
