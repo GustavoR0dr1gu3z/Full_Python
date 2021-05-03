9971c1479222623d071d950bfa43835f82ba0cb9
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-02 23:37:20 -0500

    Ejercicio 44-3

--- introduccion_Python/Diseño_Clases_Python/testOrden.py
+++ introduccion_Python/Diseño_Clases_Python/testOrden.py
@@ -4,6 +4,7 @@ from orden import Orden
 producto1 = Producto("Camisa", 100.00)
 producto2 = Producto("Pantalon",200.00)
 producto3 = Producto("Calcetines",50.00)
+producto4 = Producto("Sueter",300.00)
 
 productos = [producto1, producto2]
 
@@ -13,3 +14,8 @@ print(orden1)
 productos.append(producto3)
 orden2 = Orden(productos)
 print(orden2)
+
+orden3 = Orden(productos)
+orden3.agregar_producto(producto4)
+print(orden3)
+
