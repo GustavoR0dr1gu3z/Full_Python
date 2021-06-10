a9b104ee8aa8e8a75fc50a48cd4c6c7e5a46e8b4
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-06-09 21:56:26 -0500

    Ejercicio 45-2

--- introduccion_Python/SobreCarga_Operadores_Python/Persona.py
+++ introduccion_Python/SobreCarga_Operadores_Python/Persona.py
@@ -3,7 +3,8 @@ class Persona:
         self.nombre = nombre
     
     def __add__(self, other):
+        # Retorna el nombre del objeto y el nombre de other
+        return self.nombre + other.nombre
 
-
-ob1 + ob2
-ob1.__add__(ob2)
+# ob1 + ob2
+# ob1.__add__(ob2)
