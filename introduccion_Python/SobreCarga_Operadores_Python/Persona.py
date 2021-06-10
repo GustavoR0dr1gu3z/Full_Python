7e724a4e4d5825b121628f84cebc06cd76452cb5
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-06-09 22:00:02 -0500

    Ejercicio 45-2

--- introduccion_Python/SobreCarga_Operadores_Python/Persona.py
+++ introduccion_Python/SobreCarga_Operadores_Python/Persona.py
@@ -1,15 +1,20 @@
 class Persona:
-    def __init__(self, nombre):
+    def __init__(self, nombre, edad):
         self.nombre = nombre
+        self.edad = edad
     
     def __add__(self, other):
         # Retorna el nombre del objeto y el nombre de other
         return self.nombre + other.nombre
 
 
-p1 = Persona('Gus')
-p2 = Persona('Tavo')
+    def __sub__(self, other):
+        return self.edad - other.edad
+
+p1 = Persona('Gus',22)
+p2 = Persona('Tavo',12)
 
 print('Nombre concatenado: ',p1+p2)
+print('Resta de edades: ',p1-p2)
 # ob1 + ob2
 # ob1.__add__(ob2)
