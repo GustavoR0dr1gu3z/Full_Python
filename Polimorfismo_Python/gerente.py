f5bae9deb9eecd7b3e0d0b8f6c30d785f7fc55b8
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-06-11 20:34:24 -0500

    Ejercicio 46

--- Polimorfismo_Python/gerente.py
+++ Polimorfismo_Python/gerente.py
@@ -9,6 +9,3 @@ class Gerente(Empleado):
     def __str__(self):
         return 'Gerente {}, Departamento: {}'.format(super().__str__(), self.departamento)
 
-
-    def mostrar_detalles(self):
-        return self.__str__()
