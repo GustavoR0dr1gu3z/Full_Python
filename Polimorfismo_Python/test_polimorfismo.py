0537696743383a1f2468ae4e3ed50ed1203fce89
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-06-11 20:28:31 -0500

    Ejercicio 46

--- Polimorfismo_Python/test_polimorfismo.py
+++ Polimorfismo_Python/test_polimorfismo.py
@@ -1,3 +1,10 @@
+from empleado import Empleado 
+from gerente import Gerente 
+
 def imprimir_detalles(objeto):
     print(objeto)
     print(type(objeto))
+
+
+empleado = Empleado('Juan',5000)
+imprimir_detalles(empleado)
