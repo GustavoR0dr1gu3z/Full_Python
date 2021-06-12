8548fac93163cb8f0bd3bf8a85c2235a7c958edd
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-06-11 20:24:57 -0500

    Ejercicio 46

--- Polimorfismo_Python/gerente.py
+++ Polimorfismo_Python/gerente.py
@@ -1,10 +1,11 @@
 from empleado import Empleado 
 
 class Gerente(Empleado):
-    
+
     def __init__(self, nombre, sueldo, departamento):
         super().__init__(nombre, sueldo)
         self.departamento = departamento
 
-
+    def __str__(self):
+        return 'Gerente {}, Departamento: {}'.format(self.departamento)
 
