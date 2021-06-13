7ec7d7d658e62f78a9e51414470d8bae44917f12
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-06-12 19:10:31 -0500

    Metodo str raton

--- LABORATORIO_Mundo_PC/raton.py
+++ LABORATORIO_Mundo_PC/raton.py
@@ -7,3 +7,8 @@ class Raton(DispositivoEntrada):
     def __init__(self, marca, tipo_entrada):
         Raton.contadorRaton += 1
         self._id_raton = Raton.contadorRaton
+        super().__init__(marca,tipo_entrada)
+
+
+    def __str__(self):
+        return "ID Raton: {}, Marca: {}, Tipo_Entrada: {}".format(self._id_raton, self.marca, self.tipo_entrada)
