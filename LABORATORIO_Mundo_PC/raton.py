75bb158c808e891306435e5b756b30909d0f14f6
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-06-12 19:08:46 -0500

    contador para los ratones

--- LABORATORIO_Mundo_PC/raton.py
+++ LABORATORIO_Mundo_PC/raton.py
@@ -2,5 +2,8 @@ from dispositivoEntrada import DispositivoEntrada
 
 class Raton(DispositivoEntrada):
 
+    contadorRaton = 0
+
     def __init__(self, marca, tipo_entrada):
-        
+        Raton.contadorRaton += 1
+        self._id_raton = Raton.contadorRaton
