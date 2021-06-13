3cdc024189ca90cac0f11da86fc3ff74fbbfb038
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-06-12 19:14:52 -0500

    Probando la clase

--- LABORATORIO_Mundo_PC/dispositivoEntrada.py
+++ LABORATORIO_Mundo_PC/dispositivoEntrada.py
@@ -1,5 +1,5 @@
 class DispositivoEntrada:
 
-    def __init__(self, marca, tipo_entrada)
+    def __init__(self, marca, tipo_entrada):
         self.marca = marca
         self.tipo_entrada = tipo_entrada
