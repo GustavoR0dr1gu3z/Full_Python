be0e6edcc4c15ca2a2da28fa844fb4b8023c412d
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-06-13 21:05:35 -0500

    Metodo init

--- LABORATORIO_Mundo_PC/computadora.py
+++ LABORATORIO_Mundo_PC/computadora.py
@@ -1,4 +1,7 @@
 class Computadora: 
     contador_computadora = 0
 
-    def __init__(self, nombre, monitor, teclado, raton)
+    def __init__(self, nombre, monitor, teclado, raton):
+        Computadora.contador_computadora +=1
+        self._id_Computadora =  Computadora.contador_computadora
+        
