da42c871619bc41836dd4728662321940f60f473
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-06-13 21:01:36 -0500

    Tipos de datos

--- LABORATORIO_Mundo_PC/monitor.py
+++ LABORATORIO_Mundo_PC/monitor.py
@@ -4,12 +4,12 @@ class Monitor():
     def __init__(self, marca, tamaño):
         Monitor.contador_monitor += 1
         self._id_monitor = Monitor.contador_monitor
-        self.marca = marca
-        self.tamaño = tamaño 
+        self._marca = marca
+        self._tamaño = tamaño 
 
 
     def __str__(self):
-        return "ID Monitor: {}, Marca: {}, Tamaño: {}".format(self._id_monitor, self._marca, self.tamaño)
+        return "ID Monitor: {}, Marca: {}, Tamaño: {}".format(self._id_monitor, self._marca, self._tamaño)
 
 
 # Pruebas de la clase 
