71ca9ff955e47d831e2ad9c71144a391b80ed797
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-06-13 21:18:15 -0500

    Creando segundo objeto

--- LABORATORIO_Mundo_PC/computadora.py
+++ LABORATORIO_Mundo_PC/computadora.py
@@ -14,7 +14,7 @@ class Computadora:
         self._raton = raton 
 
     def __str__(self):
-        return "{}: {}\nMonitor: {}\nTeclado: {}\nRaton: {}".format(self._nombre, self._id_Computadora, self._monitor, self._teclado, self._raton)
+        return "{}: {}\nMonitor: {}\nTeclado: {}\nRaton: {}\n\n".format(self._nombre, self._id_Computadora, self._monitor, self._teclado, self._raton)
 
 
 if __name__ == '__main__':
