6086a6a2fceedca84abb9311ddc938e60ffec03e
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-06-13 21:12:18 -0500

    Pruebas en la clase

--- LABORATORIO_Mundo_PC/computadora.py
+++ LABORATORIO_Mundo_PC/computadora.py
@@ -11,3 +11,9 @@ class Computadora:
 
     def __str__(self):
         return "NombreComputadora: {}, ID_Computadora: {}\nMonitor: {}\nTeclado: {}\nRaton: {}".format(self._nombre, self._id_Computadora, self._monitor, self._teclado, self._raton)
+
+
+t1 = Teclado('HP', 'USB')        
+r1 = Raton('HP', 'USB')
+m1 = Monitor('HP',15)
+
