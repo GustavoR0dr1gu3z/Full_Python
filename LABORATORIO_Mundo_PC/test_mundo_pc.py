4bfb933e09ab523c66626effa916901d44a98192
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-06-13 21:27:08 -0500

    Objetos del programa

--- LABORATORIO_Mundo_PC/test_mundo_pc.py
+++ LABORATORIO_Mundo_PC/test_mundo_pc.py
@@ -4,17 +4,17 @@ from monitor import Monitor
 from computadora import Computadora
 
 # OBJETO 1
-    t1 = Teclado('HP', 'USB')        
-    r1 = Raton('HP', 'USB')
-    m1 = Monitor('HP',15)
+t1 = Teclado('HP', 'USB')        
+r1 = Raton('HP', 'USB')
+m1 = Monitor('HP',15)
 
-    c1 = Computadora('HP',m1,t1,r1)
-    print(c1)
+c1 = Computadora('HP',m1,t1,r1)
+print(c1)
 
 # OBJETO 2
-    t2 = Teclado('Acer', 'Bluetooth')        
-    r2 = Raton('Acer', 'Bluetooth')
-    m2 = Monitor('Acer',27)
+t2 = Teclado('Acer', 'Bluetooth')        
+r2 = Raton('Acer', 'Bluetooth')
+m2 = Monitor('Acer',27)
 
-    c2 = Computadora('Acer',m2,t2,r2)
-    print(c2)     
+c2 = Computadora('Acer',m2,t2,r2)
+print(c2)     
