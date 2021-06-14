8ccd4ac88c3f783aeda2664f8af54fd3c72eadc9
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-06-13 20:48:30 -0500

    Metodo init

--- LABORATORIO_Mundo_PC/teclado.py
+++ LABORATORIO_Mundo_PC/teclado.py
@@ -3,7 +3,7 @@ from dispositivoEntrada import DispositivoEntrada
 class Teclado(DispositivoEntrada):
     contador_teclado = 0
 
-    def __init__(self):
+    def __init__(self, marca, tipo_entrada):
         Teclado.contador_teclado +=1
         self._id_teclado = Teclado.contador_teclado
-        super().__init__()
+        super().__init__(marca, tipo_entrada)
