42826867fef054542317a17f150ccb6267cfddf6
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-06-13 21:15:38 -0500

    Terminando clase Computadora

--- LABORATORIO_Mundo_PC/computadora.py
+++ LABORATORIO_Mundo_PC/computadora.py
@@ -14,7 +14,7 @@ class Computadora:
         self._raton = raton 
 
     def __str__(self):
-        return "NombreComputadora: {}, ID_Computadora: {}\nMonitor: {}\nTeclado: {}\nRaton: {}".format(self._nombre, self._id_Computadora, self._monitor, self._teclado, self._raton)
+        return "{}: {}\nMonitor: {}\nTeclado: {}\nRaton: {}".format(self._nombre, self._id_Computadora, self._monitor, self._teclado, self._raton)
 
 
 t1 = Teclado('HP', 'USB')        
