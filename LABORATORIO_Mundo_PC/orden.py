d42f6cef4d79916ec0946f9785fd297b7d979586
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-06-13 21:21:02 -0500

    Metodo init

---
+++ LABORATORIO_Mundo_PC/orden.py
@@ -0,0 +1,7 @@
+class Orden:
+    contador_orden = 0
+
+    def __init__(self, computadoras):
+        Orden.contador_orden += 1
+        self._id_orden = Orden.contador_orden
+        self.computadoras = computadoras
