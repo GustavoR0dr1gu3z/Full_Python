f507dd151338fd32d7889daf1e24a14d94de4244
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-06-17 21:57:34 -0500

    Excepcion Division Entre Cero

--- Excepciones_Python/ex.py
+++ Excepciones_Python/ex.py
@@ -1,8 +1,12 @@
 # Excepciones
-
+resultado = None 
+a = 10
+b = 0
 
 # Division entre 0
 try:
-    10/0
+    resultado = a/b
 except Exception as e:
     print("Dividion entre cero, no se puede: {}".format(e))
+
+print("Resultado: {} ".format(resultado))
