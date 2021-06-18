407e70e63f4f8b4691a4d9360b8fdfe848a26e27
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-06-17 22:08:28 -0500

    Excepciones especificas

--- Excepciones_Python/ex.py
+++ Excepciones_Python/ex.py
@@ -1,10 +1,9 @@
 # Excepciones
 resultado = None 
-a = '10'
-b = 0
 
-# Division entre 0
 try:
+    a = '10'
+    b = 0
     resultado = a/b
 except ZeroDivisionError as e:
     print("Ocurrio un error: {}, {}".format(e, type(e)))
