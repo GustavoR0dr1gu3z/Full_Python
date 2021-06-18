7806453923dbbadc4e46c1be58930f5fcb2a1682
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-06-17 22:12:10 -0500

    Excepciones especificas

--- Excepciones_Python/ex.py
+++ Excepciones_Python/ex.py
@@ -5,14 +5,14 @@ try:
     a = int(input("Primer numero: "))
     b = int(input("Segundo numero: "))
     resultado = a/b
-'''    
+    '''    
 except ZeroDivisionError as e:
     print("Ocurrio un error: {}, {}".format(e, type(e)))
 except TypeError as e:
     print("Ocurrio un error: {}, {}".format(e, type(e)))
 except ValueError as e:
     print("Ocurrio un error: {}, {}".format(e, type(e)))
-'''
+    '''
 except  Exception as e:
     print("Ocurrio un error: {}, {}".format(e, type(e)))
 
